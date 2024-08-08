b5559b5154ccdac02f17560847dc59769df8041a
Esteban Bosse <estebanbosse@gmail.com>
2024-08-08 23:00:47 +0200

    add first example of get with rust struct to json

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{response::Json, routing::get, Router};
+
+mod package;
 
 #[tokio::main]
 async fn main() {
     // build our application with a route
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new().route("/packages", get(get_packages));
 
     // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
@@ -13,6 +15,7 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+async fn get_packages() -> Json<package::Package> {
+    let p = package::Package{name: "python3".to_string(), version: "1.2.3".to_string(), hash: "aoaeuaoue".to_string()};
+    Json(p)
 }
